0644d321b204dd1c4ee1f1557f68caca353438ec
Victor Augusto <victoraweb@gmail.com>
2019-02-28 22:01:54 -0300

    Initial commit

---
+++ src/main/kotlin/augustovictor/mockklab/mockklab/MockkLabApplication.kt
@@ -0,0 +1,11 @@
+package augustovictor.mockklab.mockklab
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MockkLabApplication
+
+fun main(args: Array<String>) {
+    runApplication<MockkLabApplication>(*args)
+}
